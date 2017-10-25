b7d45a238f48460cbed386d7730e2b596f1542e3
Sam Minot <sam@fredhutch.org>
2017-10-25 09:53:20 -0700

    Make project config files

--- README.md
+++ README.md
@@ -15,18 +15,20 @@ The format of each project config file is as follows:
 {
 	"name": "HMP UniRef90 Sept 26 2017",
 	"description": "Analysis of HMP datasets against the UniRef90 database",
-	"job_definition": "diamond:1",
-	"db": "s3://bucket/db_folder/database.dmnd",
-	"output_folder": "s3://bucket/output_folder/",
-	"queue": "queue_16cpu_120G",
-	"samples": ["SRA12345", ...],
-	"status": "SUBMITTED", # or COMPLETED, added after job submission
-	"jobs": ["abcd0123-456e-7f89-01g2-345hi6789j01", ...]  # added after job submission
+	"job_definition": "diamond:1",                          # Name of job definition on AWS Batch
+	"db": "s3://bucket/db_folder/database.dmnd",            # Reference database for analysis
+	"output_folder": "s3://bucket/output_folder/",          # Output placed in this folder after completion
+	"queue": "queue_16cpu_120G",                            # Name of AWS Batch queue
+	"samples": ["sra://SRR12345", ...],                     # Location of samples to analyze
+	"status": "SUBMITTED",                                  # or COMPLETED, added after job submission
+	"jobs": ["abcd0123-456e-7f89-01g2-345hi6789j01", ...],  # added after job submission
+	"parameters": []                                        # Optional set of parameters to pass to the jobs
 }
 ```
 
 
 ### Scripts
 
+  * make_project_jsons.py: Create a project config file from an analysis template
   * project-helper.py: Used to submit and monitor the status of projects on AWS Batch
   * run-and-watch.py: Run a command in Batch and watch for the output
---
+++ scripts/make_project_jsons.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import os
+import sys
+import json
+import boto3
+import pandas as pd
+
+# USAGE
+
+# Arguments:
+# 1. Analysis parameters (skeleton of analysis config object)
+# 2. Metadata CSV with "Run_s" column containing a list of SRA accessions
+# 3. Name for the project (determines the output location)
+
+# Path to the skeleton of the job definition
+skel_fp = sys.argv[1]
+metadata_fp = sys.argv[2]
+project_name = sys.argv[3]
+
+assert os.path.exists(skel_fp)
+assert os.path.exists(metadata_fp)
+
+# Read in the skeleton for the project definition
+project = json.load(open(skel_fp))
+
+# OUTPUT LOCATION
+# The following describes the path where all output files will be placed
+bucket = "fh-pi-fredricks-d"
+prefix = "lab/Sam_Minot/data/{}_diamond_uniref90/".format(project_name)
+
+
+def aws_s3_ls(bucket, prefix):
+    """List the contents of an S3 bucket."""
+
+    # Connect to S3
+    client = boto3.client('s3')
+
+    # Get all of the objects from S3
+    tot_objs = []
+    # Retrieve in batches of 1,000
+    objs = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+
+    continue_flag = True
+    while continue_flag:
+        continue_flag = False
+
+        if 'Contents' not in objs:
+            break
+
+        # Add this batch to the list
+        tot_objs.extend(objs['Contents'])
+
+        # Check to see if there are more to fetch
+        if objs['IsTruncated']:
+            continue_flag = True
+            token = objs['NextContinuationToken']
+            objs = client.list_objects_v2(Bucket=bucket,
+                                          Prefix=prefix,
+                                          ContinuationToken=token)
+    return [d["Key"].split('/')[-1] for d in tot_objs]
+
+
+# Number of samples per process
+n_samples = 20
+
+# Read in the metadata
+df = pd.read_table(metadata_fp, sep=',')
+# Get the list of SRA accessions for each individual sample
+sra_accessions = list(df['Run_s'].values)
+sra_accessions = ["sra://" + acc for acc in sra_accessions]
+
+# Format the URL on S3 for the output files
+fp = "s3://{}/{}".format(bucket, prefix)
+project["output_folder"] = fp
+
+# Get the contents of the output folder
+compl = aws_s3_ls(bucket, prefix)
+
+# Remove the samples that have already been completed
+print("Project: " + project_name)
+print("Total samples: {}".format(len(sra_accessions)))
+print("Files in output folder: {}".format(len(compl)))
+sra_accessions = [s for s in sra_accessions
+                  if s.replace("sra://", "") + ".json.gz" not in compl]
+print("Unanalyzed samples: {}".format(len(sra_accessions)))
+
+# Add the samples to the project config object
+project['samples'] = []
+
+# Add the samples in batches
+while len(sra_accessions) > 0:
+    n = min(len(sra_accessions), n_samples)
+
+    project['samples'].append(','.join(sra_accessions[:n]))
+
+    sra_accessions = sra_accessions[n:]
+
+# Write out the config file
+fp_out = '{}.json'.format(project_name)
+with open(fp_out, 'wt') as fo:
+    json.dump(project, fo, indent=4)
+
+print("Wrote out {}".format(fp_out))
+print("Analysis can be kicked off with: ")
+print("project-helper.py submit {}".format(fp_out))
