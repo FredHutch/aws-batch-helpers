9d61b24042a80187170798a9ce0e83098edb37b3
Sam Minot <sam@fredhutch.org>
2018-04-10 18:18:34 -0700

    Add helpers

---
+++ batch_helpers/helpers.py
@@ -0,0 +1,60 @@
+import sys
+import shutil
+import logging
+import traceback
+import subprocess
+
+def exit_and_clean_up(temp_folder):
+    """Log the error messages and delete the temporary folder."""
+    # Capture the traceback
+    logging.info("There was an unexpected failure")
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    for line in traceback.format_tb(exc_traceback):
+        logging.info(line)
+
+    # Delete any files that were created for this sample
+    logging.info("Removing temporary folder: " + temp_folder)
+    shutil.rmtree(temp_folder)
+
+    # Exit
+    logging.info("Exit type: {}".format(exc_type))
+    logging.info("Exit code: {}".format(exc_value))
+    sys.exit(exc_value)
+
+
+def run_cmds(commands, retry=0, catchExcept=False, stdout=None):
+    """Run commands and write out the log, combining STDOUT & STDERR."""
+    logging.info("Commands:")
+    logging.info(' '.join(commands))
+    if stdout is None:
+        p = subprocess.Popen(commands,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+        stdout, stderr = p.communicate()
+    else:
+        with open(stdout, "wt") as fo:
+            p = subprocess.Popen(commands,
+                                 stderr=subprocess.PIPE,
+                                 stdout=fo)
+            stdout, stderr = p.communicate()
+        stdout = False
+    exitcode = p.wait()
+    if stdout:
+        logging.info("Standard output of subprocess:")
+        for line in stdout.decode("latin-1").split('\n'):
+            logging.info(line)
+    if stderr:
+        logging.info("Standard error of subprocess:")
+        for line in stderr.decode("latin-1").split('\n'):
+            logging.info(line)
+
+    # Check the exit code
+    if exitcode != 0 and retry > 0:
+        msg = "Exit code {}, retrying {} more times".format(exitcode, retry)
+        logging.info(msg)
+        run_cmds(commands, retry=retry - 1)
+    elif exitcode != 0 and catchExcept:
+        msg = "Exit code was {}, but we will continue anyway"
+        logging.info(msg.format(exitcode))
+    else:
+        assert exitcode == 0, "Exit code {}".format(exitcode)
