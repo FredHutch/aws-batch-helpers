a1434e03a3c60b742d5684f85628d7de76441417
Sam Minot <sam@fredhutch.org>
2018-07-09 12:57:04 -0700

    Helper functions to write JSON and check for S3 objects

--- batch_helpers/helpers.py
+++ batch_helpers/helpers.py
@@ -1,4 +1,6 @@
 import sys
+import json
+import boto3
 import shutil
 import logging
 import traceback
@@ -58,3 +60,26 @@ def run_cmds(commands, retry=0, catchExcept=False, stdout=None):
         logging.info(msg.format(exitcode))
     else:
         assert exitcode == 0, "Exit code {}".format(exitcode)
+
+
+def s3_path_exists(s3_path):
+    """Check if a path exists on S3."""
+    s3 = boto3.resource('s3')
+    assert s3_path.startswith("s3://")
+    bucket, key = s3_path[5:].split("/", 1)
+    bucket = s3.Bucket(bucket)
+    objs = list(bucket.objects.filter(Prefix=key))
+    if len(objs) > 0 and objs[0].key == key:
+        return True
+    else:
+        return False
+
+    
+def write_s3_json(dat, s3_path):
+    """Write some data in JSON format to S3."""
+    s3 = boto3.resource('s3')
+    dat_json = json.dumps(dat)
+    assert s3_path.startswith("s3://")
+    bucket, key = s3_path[5:].split("/", 1)
+    object = s3.Object(bucket, key)
+    object.put(Body=dat_json)
